1e2af0902176e8225a6a17ebfa39558c9f93dd9a
Lilly Liu <lillyliu@Lillys-MacBook-Air.local>
2022-04-02 15:01:52 -0700

    added custom vocab

--- pytranscribe/initiate_transcription.py
+++ pytranscribe/initiate_transcription.py
@@ -15,8 +15,10 @@ def initiate_transcription(file_id):
     transcription, so there is a different function to retrieve
     the results.
     """
+    custom = ["Julie", "Robin", "Livingroom", "Bedroom", "Bathroom", "Garden", "Barn", "tackroom", "drink", "tap", "follow", "books", "movies", "sport", "travel", "fire", "light", "records", "door", "records", "painting", "piano", "window", "music", "song", "tune", "chocolates", "fan", "outfit", "closet", "bed", "carpet", "TV", "television", "cat", "couch", "recliner", "seat", "lamp", "pillows", "blankets", "clothing", "hat", "sunglasses", "medication", "pill", "music", "teeth", "brush", "pajamas", "sleep", "freshen", "clean", "toothpaste", "gums", "heater", "bathrobe", "shower", "curtain", "toilet", "washcloth", "cream", "moisturize", "nail", "polish", "deodorant", "perfume", "lipstick", "powder", "pencil", "eyebrows", "blow", "dryer", "drawer", "shaving", "shave", "razor", "comb", "scale", "mirror", "sink", "water", "flower", "carrot", "vegetables", "dirt", "plants", "lettuce", "tomato", "tomatoes", "tree", "sun", "sunny", "birdbath", "bird", "bucket", "apple", "kitty", "shade", "radio", "barn", "stall", "starr", "grooming", "groom", "horse", "work", "dixie", "pasture", "touch", "dirt", "fur", "curry", "comb", "coat", "mane", "shine", "body", "face", "halter", "rope", "strokes", "fred", "dog", "rosie", "goat", "gate"]
     endpoint = "".join([API_URL, "transcript"])
-    json = {"audio_url": "".join([CDN_URL, "upload/{}".format(file_id)])}
+    json = {"audio_url": "".join([CDN_URL, "upload/{}".format(file_id)]), "disfluencies": True,
+    "word_boost": custom}
     headers = {
         "authorization": os.getenv("ASSEMBLYAI_KEY"),
         "content-type": "application/json"
